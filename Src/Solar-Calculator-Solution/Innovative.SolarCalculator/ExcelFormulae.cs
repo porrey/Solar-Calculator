4270f20a2257749532847c4b1b1b1185b5618615
Daniel Porrey <porrd0bl@prscoad.com>
2020-01-24 20:32:45 -0600

    Updated file headers.

--- Src/Solar-Calculator-Solution/Innovative.SolarCalculator/ExcelFormulae.cs
+++ Src/Solar-Calculator-Solution/Innovative.SolarCalculator/ExcelFormulae.cs
@@ -1,18 +1,20 @@
 ﻿// ***
-// *** Copyright (C) 2013-2020, Daniel M. Porrey. All rights reserved.
-// *** Written By Daniel M. Porrey
-// ***
-// *** This software is provided "AS IS," without a warranty of any kind. ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES, 
-// *** INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED. DANIEL M PORREY 
-// *** AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE 
-// *** OR ITS DERIVATIVES. IN NO EVENT WILL DANIEL M PORREY OR ITS LICENSORS BE LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, 
-// *** SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF 
-// *** OR INABILITY TO USE THIS SOFTWARE, EVEN IF DANIEL M PORREY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES. 
-// ***
-// *** Licensed under Microsoft Reciprocal License (Ms-RL)
-// *** This license governs use of the accompanying software. If you use the software, you accept this license. If you do not accept the license, 
-// *** do not use the software. Full license details can be found at https://raw.githubusercontent.com/porrey/Solar-Calculator/master/LICENSE.
-// ***
+// *** Solar Calculator 3.0.0
+// *** Copyright(C) 2013-2020, Daniel M. Porrey. All rights reserved.
+// *** 
+// *** This program is free software: you can redistribute it and/or modify
+// *** it under the terms of the GNU General Public License as published by
+// *** the Free Software Foundation, either version 3 of the License, or
+// *** (at your option) any later version.
+// *** 
+// *** This program is distributed in the hope that it will be useful,
+// *** but WITHOUT ANY WARRANTY; without even the implied warranty of
+// *** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+// *** GNU General Public License for more details.
+// *** 
+// *** You should have received a copy of the GNU General Public License
+// *** along with this program.If not, see<http://www.gnu.org/licenses/>.
+// *** 
 using System;
 
 namespace Innovative.SolarCalculator
