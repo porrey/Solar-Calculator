00582b4d3212a373dba31e64d4937d43703d6b8a
Daniel Porrey <porrey@users.noreply.github.com>
2020-04-14 08:27:49 -0500

    Update NuGet.Publish.cmd

--- Src/NuGet.Publish.cmd
+++ Src/NuGet.Publish.cmd
@@ -13,7 +13,7 @@ SET /P PUBLISH_KEY=Enter the publish key:
 REM ***
 REM *** Publish all *.nupkg files.
 REM ***
-forfiles /S /M *.nupkg /C "cmd /c dotnet nuget push @path -k %PUBLISH_KEY% -s https://api.nuget.org/v3/index.json"
+forfiles /P "%CD%" /S /M *.nupkg /C "cmd /c dotnet nuget push @path -k %PUBLISH_KEY% -s https://api.nuget.org/v3/index.json"
 
 REM ***
 REM *** Pause to view errors.
